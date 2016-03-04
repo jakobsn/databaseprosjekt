66d33f184e9dfb1ae63097d3ae24541c8da2761a
Kristoffer Thorset <HerrBab@dhcp-028078.wlan.ntnu.no>
2016-03-04 11:59:34 +0100

    Tables

--- dbprogram/src/dbprogram/JDBCMySQLConnection.java
+++ dbprogram/src/dbprogram/JDBCMySQLConnection.java
@@ -1,5 +1,40 @@
 package dbprogram;
 
-public class JDBCMySLConnection {
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+public class JDBCMySQLConnection {
+    //static reference to itself
+    private static JDBCMySQLConnection instance = new JDBCMySQLConnection();
+    public static final String URL = "treningsdagbok:mysql://localhost/treningsdagbok";
+    public static final String USER = "root";
+    public static final String PASSWORD = "ribbefett";
+    public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver"; 
+     
+    //private constructor
+    private JDBCMySQLConnection() {
+        try {
+            //Step 2: Load MySQL Java driver
+            Class.forName(DRIVER_CLASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+     
+    private Connection createConnection() {
+ 
+        Connection connection = null;
+        try {
+            //Step 3: Establish Java MySQL connection
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("ERROR: Unable to Connect to Database.");
+        }
+        return connection;
+    }   
+     
+    public static Connection getConnection() {
+        return instance.createConnection();
+    }
 }
